48f48fbafc8bad10dace5cfd4cb092e391e49e1b
vikblom <veblomqvist@gmail.com>
2017-04-14 12:37:06 +0200

    Added a script for scraping down motion .txts

---
+++ riksmot/__init__.py
@@ -0,0 +1,4 @@
+import os
+
+pkg_dir, pkg_file = os.path.split(__file__)
+DATA_PATH = os.path.join(pkg_dir, "..", "motioner")
---
+++ scripts/get_motioner.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+
+import os
+import requests
+import re
+from zipfile import ZipFile
+from io import BytesIO, StringIO
+
+from riksmot import DATA_PATH
+
+
+if not os.path.isdir(DATA_PATH):
+    os.mkdir(DATA_PATH)
+
+print("Dowloading motioner into:\n\t{}".format(DATA_PATH))
+
+riksdag_doc_url = "http://data.riksdagen.se/data/dokument/"
+content = requests.get(riksdag_doc_url).content
+match = re.findall('href="([a-z./]+mot[0-9\-]+\.text\.zip)',
+                   str(content))
+
+print("Getting archives")
+for zip_url in match:
+    print("\thttp:" + zip_url)
+    zip_content = requests.get("http:" + zip_url).content
+
+    with ZipFile(BytesIO(zip_content)) as zip:
+        zip.extractall(DATA_PATH)
+
+print("Done")
